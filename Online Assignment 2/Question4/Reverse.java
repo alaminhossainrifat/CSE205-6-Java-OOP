cbaa06fe1bb1dbc65e8018413aef57e6c2fd7281
alaminhossainrifat <rif420aat@gmail.com>
2024-10-12 16:34:17 +0600

    A2 Q4

---
+++ Online Assignment 2/Question4/Reverse.java
@@ -0,0 +1,25 @@
+package Question4;
+
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        int reversed = 0;
+        
+        while (number != 0) {
+            int digit = number % 10; 
+            reversed = reversed * 10 + digit; 
+            number /= 10; 
+        }
+        
+        System.out.println("Reversed Number: " + reversed);
+        
+        scanner.close();
+    }
+}
+
